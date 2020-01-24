e69aac85e1ffb60a6e8fafcf57d0977a980e01cc
李宜雯 <liy721511@gmail.com>
2020-01-24 08:57:21 +0800

    init commit

---
+++ app/src/main/java/com/example/imagepicker/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.imagepicker
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    var saveUri:Uri? = null
+
+    private companion object {
+        val PHOTO_FROM_GALLERY = 0
+        val PHOTO_FROM_CAMERA = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            saveUri = Uri.parse(savedInstanceState.getString("saveUri"))
+        }
+
+        permission()
+
+        toAlbum.setOnClickListener {
+            toAlbum()
+        }
+
+        toCamera.setOnClickListener {
+            toCamera()
+        }
+
+    }
+
+    fun permission(){
+
+        val permissionList = arrayListOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        var size = permissionList.size
+        var i = 0
+        while (i < size) {
+            if (ActivityCompat.checkSelfPermission(this, permissionList[i]) == PackageManager.PERMISSION_GRANTED) {
+                permissionList.removeAt(i)
+                i -= 1
+                size -= 1
+            }
+            i += 1
+        }
+        val array = arrayOfNulls<String>(permissionList.size)
+        if (permissionList.isNotEmpty()) ActivityCompat.requestPermissions(this, permissionList.toArray(array), 0)
+    }
+    fun toAlbum() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, PHOTO_FROM_GALLERY)
+    }
+
+    fun toCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val tmpFile = File(Environment.getExternalStorageDirectory().toString(), System.currentTimeMillis().toString() + ".jpg")
+        val uriForCamera = FileProvider.getUriForFile(this, "com.example.imagepicker.fileprovider", tmpFile)
+
+        saveUri = uriForCamera
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriForCamera)
+        startActivityForResult(intent, PHOTO_FROM_CAMERA)
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if(saveUri != null){
+            val uriString = saveUri.toString()
+            outState.putString("saveUri", uriString)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            PHOTO_FROM_GALLERY -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        val uri = data!!.data
+                        imageView.setImageURI(uri)
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        Log.wtf("getImageResult", resultCode.toString())
+                    }
+                }
+            }
+
+            PHOTO_FROM_CAMERA -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        Glide.with(this).load(saveUri).into(imageView)
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        Log.wtf("getImageResult", resultCode.toString())
+                    }
+                }
+
+            }
+        }
+    }
+
+    fun dailog(){
+        AlertDialog.Builder(this)
+            .setTitle("提醒")
+            .setMessage("相機功能將無法使用")
+    }
+
+
+}
